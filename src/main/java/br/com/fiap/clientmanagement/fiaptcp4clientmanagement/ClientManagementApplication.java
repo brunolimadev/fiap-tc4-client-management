23a9aa8e11adb312bed62315f9a95fad62ec7320
EricRangelRJ <b.ericrangel@hotmail.com>
2024-04-22 22:34:18 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/fiap/clientmanagement/fiaptcp4clientmanagement/ClientManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.clientmanagement.fiaptcp4clientmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/clientmanagement/fiaptcp4clientmanagement/FiapTcp4ClientManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.clientmanagement.fiaptcp4clientmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapTcp4ClientManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
