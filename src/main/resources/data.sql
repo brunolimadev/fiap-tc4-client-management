f2e8feb55ce20ec4e4e9af13912f8074d9ac1379
Eric Rangel <b.ericrangel@hotmail.com>
2024-05-01 23:39:00 -0300

    feat | add load on tables

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+----Carga de Endereços
+INSERT INTO address (id, city, complement, country, neighborhood, number,province, street) VALUES ('d27ee6d3-02da-445c-a3a2-7e1f5ff9e138','Rio de Janeiro','Rua do hospital da mãe','Brasil','Próximo ao hospital ','180','Mesquita','Rua Meriti');
+INSERT INTO address (id, city, complement, country, neighborhood, number,province, street) VALUES ('d27ee6d3-02da-445c-a3a2-7e1f5ff9e148','Rio de Janeiro','Rua do hospital da mãe','Brasil','Próximo ao hospital ','180','Mesquita','Rua Meriti');
+INSERT INTO address (id, city, complement, country, neighborhood, number,province, street) VALUES ('d27ee6d3-02da-445c-a3a2-7e1f5ff9e158','Rio de Janeiro','Rua do hospital da mãe','Brasil','Próximo ao hospital ','180','Mesquita','Rua Meriti');
+INSERT INTO address (id, city, complement, country, neighborhood, number,province, street) VALUES ('d27ee6d3-02da-445c-a3a2-7e1f5ff9e168','Rio de Janeiro','Rua do hospital da mãe','Brasil','Próximo ao hospital ','180','Mesquita','Rua Meriti');
+INSERT INTO address (id, city, complement, country, neighborhood, number,province, street) VALUES ('d27ee6d3-02da-445c-a3a2-7e1f5ff9e178','Rio de Janeiro','Rua do hospital da mãe','Brasil','Próximo ao hospital ','180','Mesquita','Rua Meriti');
+INSERT INTO address (id, city, complement, country, neighborhood, number,province, street) VALUES ('d27ee6d3-02da-445c-a3a2-7e1f5ff9e188','Rio de Janeiro','Rua do hospital da mãe','Brasil','Próximo ao hospital ','180','Mesquita','Rua Meriti');
+INSERT INTO address (id, city, complement, country, neighborhood, number,province, street) VALUES ('d27ee6d3-02da-445c-a3a2-7e1f5ff9e198','Rio de Janeiro','Rua do hospital da mãe','Brasil','Próximo ao hospital ','180','Mesquita','Rua Meriti');
+--
+----Carga de Clientes
+INSERT INTO client (id, email, name, address) VALUES ('d17ee6d3-02da-440c-a3a2-7e1f5ff9e138','ericlsrangel@gmail.com','Eric Leonardo S. Rangel','d27ee6d3-02da-445c-a3a2-7e1f5ff9e138');
+INSERT INTO client (id, email, name, address) VALUES ('d17ee6d3-02da-441c-a3a2-7e1f5ff9e138','brunarangel@gmail.com','Bruna Nunes Rangel','d27ee6d3-02da-445c-a3a2-7e1f5ff9e148');
+INSERT INTO client (id, email, name, address) VALUES ('d17ee6d3-02da-442c-a3a2-7e1f5ff9e138','enzorangel@gmail.com','Enzo Davi da Costa Rangel','d27ee6d3-02da-445c-a3a2-7e1f5ff9e158');
+INSERT INTO client (id, email, name, address) VALUES ('d17ee6d3-02da-443c-a3a2-7e1f5ff9e138','eloarangel@gmail.com','Eloá da Costa Rangel','d27ee6d3-02da-445c-a3a2-7e1f5ff9e168');
+INSERT INTO client (id, email, name, address) VALUES ('d17ee6d3-02da-444c-a3a2-7e1f5ff9e138','augustorangel@gmail.com','José Augusto Costa Rangel','d27ee6d3-02da-445c-a3a2-7e1f5ff9e178');
+INSERT INTO client (id, email, name, address) VALUES ('d17ee6d3-02da-445c-a3a2-7e1f5ff9e138','elianesrangel@gmail.com','Eliane Moura Rangel','d27ee6d3-02da-445c-a3a2-7e1f5ff9e188');
+INSERT INTO client (id, email, name, address) VALUES ('d17ee6d3-02da-446c-a3a2-7e1f5ff9e138','evertonlsrangel@gmail.com','Everton Leandro S. Rangel','d27ee6d3-02da-445c-a3a2-7e1f5ff9e198');
